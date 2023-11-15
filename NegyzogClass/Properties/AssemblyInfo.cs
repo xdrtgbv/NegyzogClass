a4a731b0f445ef0c1663f972fe0d847c9f1acedd
Norbert Győrfi <2020074@dszcberegszaszi.hu>
2023-11-15 11:47:58 +0100

    Add project files.

---
+++ NegyzogClass/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NegyzogClass")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NegyzogClass")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("82c144b0-ad58-4e4d-a8f7-c356417afed7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NegyzogClass/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Kocka;
+
+namespace KockaUnitTeszt
+{
+    [TestClass]
+    public class NegyszogTeszteles
+    {
+        [TestMethod]
+        public void TeruletTeszt()
+        {
+            NegyszogClass negyszog1 = new NegyszogClass();
+            negyszog1.SetTerulet();
+            double varteredmeny = 11;
+            double kapotteredmeny = negyszog1.GetTerulet();
+            Assert.AreEqual(varteredmeny, kapotteredmeny);
+
+        }
+        [TestMethod]
+        public void KeruletTeszt()
+        {
+            NegyszogClass negyszog1 = new NegyszogClass();
+            negyszog1.SetKerulet();
+            double varteredmeny = 3;
+            double kapotteredmeny = negyszog1.GetKerulet();
+            Assert.AreEqual(varteredmeny, kapotteredmeny);
+        }
+    }
+}
